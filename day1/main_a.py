3f04858421fd5cb35f255768d59fc11f27e748aa
Rens <renshamburger@gmail.com>
2024-12-01 12:45:05 +0100

    Code format a

--- day1/main_a.py
+++ day1/main_a.py
@@ -1,5 +1,7 @@
 print(
-    sum([abs(i-j) for i, j in zip(
-        *list(map(sorted, list(zip(*[[int(l.split('   ')[0]), int(l.split('   ')[1])] for l in open('input/input').readlines()])))))
-         ])
+    sum(
+        map(lambda i, j: abs(i-j), *map(
+            sorted, zip(*[[int(l.split('   ')[0]), int(l.split('   ')[1])] for l in open('input/input').readlines()])
+        ))
+    )
 )
